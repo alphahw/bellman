4d5d6e8b27be304c257064f65b02f62a186b74a2
Albin Halinen Wilén <albin_wilen@msn.com>
2015-02-16 11:27:06 -0500

    Commenting, refactoring, and module exportin'. (Untested, might be b0rken now 😮)

--- app.js
+++ app.js
@@ -7,73 +7,82 @@ var request = require('request'),
 var lastCurrentTrackMetaData = null,
 	lastPlaying = {};
 
-// Let's listen to the sounds… err, UPnP notifications… of the SONOS.
+var Bellman = {};
 
-var x = new Listener(new Sonos(process.env.SONOS_HOST || '192.168.0.0'));
+// Let's listen to the sounds… err, UPnP notifications, of the SONOS.
 
-x.listen(function(err) {
+Bellman.listen() {
 	
-	if (err) throw err;
-  
-	x.addService('/MediaRenderer/AVTransport/Event', function(error, sid) {
-		if (error) throw err;
+	var soundMachine = new Listener(new Sonos(process.env.SONOS_HOST || '192.168.0.0'));
 
-		// Subscribing so we'll get notified every now and then
+		soundMachine.listen(function(err) {	
 
-		console.log('Successfully subscribed, with subscription id', sid, '\n');
-	});
+		if (err) {
+			throw "[" + new Date() + "] Bellman couldn't listen to SONOS:\n" + err;
+	  	}
 
-	x.on('serviceEvent', function(endpoint, sid, data) {
-		//console.log('Received event from', endpoint, '(' + sid + ') with data:', data, '\n\n');
+		Bellman.addService('/MediaRenderer/AVTransport/Event', function(error, sid) {
+			if (error) throw err;
 
-		console.log('– – – – – – – – – – – – – – – – – – – – – –\nReceived event from', endpoint, '(' + sid + ')\n');
+			// Subscribing so we'll get notified every now and then
 
-		// Ooh, we got a notification! Let's poke inside…
-
-		xml2js.parseString(data.LastChange, function(err, result) {
-			//console.log(util.inspect(result, false, null));
-
-			// First xml2js(ON) pass – however, the CurrentTrackMetaData val is still XML (well, DIDL), so we have to run that through the parser, if there's a new track playing
-
-			var annoyingJSONPathToMetaData = result.Event.InstanceID[0].CurrentTrackMetaData[0].$.val;
-
-			console.log('Parsed JSON still in form of DIDL: ' + annoyingJSONPathToMetaData + '\n');
-
-			if (lastCurrentTrackMetaData != annoyingJSONPathToMetaData) {
-
-				// Did the current track change? If so, parse the data and pull out goodies
-
-				lastCurrentTrackMetaData = annoyingJSONPathToMetaData;
-
-				xml2js.parseString(annoyingJSONPathToMetaData, function(err, result) {
-					//console.log(util.inspect(result, false, null));
-
-					// Second xml2js(ON) pass – now we can grab the nice stuff inside! See DIDL parser below.
-
-					var currentTrackMetaData = parseDIDL(result);
-
-					//console.log(util.inspect(currentTrackMetaData, false, null));
-
-					checkForNewTrack(currentTrackMetaData);
-
-				});
-
-			} else {
-
-				// No change? Do nothing, except log it.
-
-				console.log('No track change. (Unparsed DIDL level detection. Shallow!)\n');
-
-			}
-
+			console.log('[' + new Date() + '] Successfully subscribed, with subscription id', sid, '\n');
 		});
 
+		Bellman.on('serviceEvent', function(endpoint, sid, data) {
+			//console.log('Received event from', endpoint, '(' + sid + ') with data:', data, '\n\n');
+
+			console.log('– – – – – – – – – – – – – – – – – – – – – –\n[' + new Date() + '] Received event from', endpoint, '(' + sid + ')\n');
+
+			// Ooh, we got a notification! Let's poke inside…
+
+			xml2js.parseString(data.LastChange, function(err, result) {
+				//console.log(util.inspect(result, false, null));
+
+				// First xml2js(ON) pass – however, the CurrentTrackMetaData val is still XML (well, DIDL), so we have to run that through the parser, if there's a new track playing
+
+				var annoyingJSONPathToMetaData = result.Event.InstanceID[0].CurrentTrackMetaData[0].$.val;
+
+				console.log('[' + new Date() + '] Parsed JSON still in form of DIDL:\n' + annoyingJSONPathToMetaData + '\n');
+
+				if (lastCurrentTrackMetaData != annoyingJSONPathToMetaData) {
+
+					// Did the current track change? If so, parse the data and pull out goodies
+
+					lastCurrentTrackMetaData = annoyingJSONPathToMetaData;
+
+					xml2js.parseString(annoyingJSONPathToMetaData, function(err, result) {
+						//console.log(util.inspect(result, false, null));
+
+						// Second xml2js(ON) pass – now we can grab the nice stuff inside! See DIDL parser below.
+
+						var currentTrackMetaData = parseDIDL(result);
+
+						//console.log(util.inspect(currentTrackMetaData, false, null));
+
+						Bellman.checkForNewTrack(currentTrackMetaData);
+
+					});
+
+				} else {
+
+					// No change? Do nothing, except log it.
+
+					console.log('[' + new Date() + '] No track change. (Unparsed DIDL level detection; shallow.)\n');
+
+				}
+
+			});
+
+		});
+	
 	});
-});
+
+}
 
 // Verify there's a new track playing
 
-function checkForNewTrack(currentlyPlaying) {
+Bellman.checkForNewTrack(currentlyPlaying) {
 
 	// If there's nothing in the lastPlaying object, we fill it out…
 
@@ -82,22 +91,22 @@ function checkForNewTrack(currentlyPlaying) {
 		lastPlaying.album == null &&
 		lastPlaying.albumArtURI == null) {
 
-		// Yes, not the easiest way to do this – but we're set in case the keys should ever change (needs to be done to the DIDL parser too though)
+		// Yes, not the easiest way to do this – but we're set in case the keys/structure should ever change (needs to be done to the DIDL parser too though)
 
 		lastPlaying.title = currentlyPlaying.title;
 		lastPlaying.artist = currentlyPlaying.artist;
 		lastPlaying.album = currentlyPlaying.album;
 		lastPlaying.albumArtURI = currentlyPlaying.albumArtURI;
 
-		console.log('Filled out null lastPlaying fields.\n');
+		console.log('[' + new Date() + '] Filled out null lastPlaying fields.\n');
 
 		// And send the whole thing to Slack!
 
-		sendToSlack(lastPlaying);
+		Bellman.sendToSlack(lastPlaying);
 
 	} else {
 
-	// …else, we check if it's the same song - no need to anything in that case.
+	// …else, we check if it's the same song - no need to anything in that case. (However, said non-affecting else block should theoretically never fire now that song change is checked earlier before the DIDL is parsed.)
 
 		if ((lastPlaying.title != currentlyPlaying.title) || (
 			currentlyPlaying.title != null &&
@@ -114,11 +123,11 @@ function checkForNewTrack(currentlyPlaying) {
 
 			// And send the whole thing to Slack!
 
-			sendToSlack(lastPlaying);
+			Bellman.sendToSlack(lastPlaying);
 
 		} else {
 
-			console.log('No track change. (Parsed JSON level detection. Deep!)\n');
+			console.log('[' + new Date() + '] No track change. (Parsed JSON level detection; deep.)\n');
 
 		}
 
@@ -128,16 +137,20 @@ function checkForNewTrack(currentlyPlaying) {
 
 // Posting the result to Slack
 
-function sendToSlack(trackMetadata) {
-
-	var artistAndTitle = '*' + trackMetadata.artist + ' – ' + trackMetadata.title + '*';
-	var album = '_' + trackMetadata.album + '_';
+Bellman.sendToSlack(trackMetadata) {
 
 	var payload = {};
 
 	if (trackMetadata.album != null && trackMetadata.album != 'null') {
+		// Formatting for bold artist/title line, and italic album line (Slack markdown)
+		var artistAndTitle = '*' + trackMetadata.artist + ' – ' + trackMetadata.title + '*';
+		var album = '_' + trackMetadata.album + '_';
+
 		payload.text = artistAndTitle + '\n' + album;
 	} else {
+		// If no album, only show artist and title
+		var artistAndTitle = '*' + trackMetadata.artist + ' – ' + trackMetadata.title + '*';
+
 		payload.text = artistAndTitle;
 	}
 
@@ -156,8 +169,8 @@ function sendToSlack(trackMetadata) {
 
 }
 
-// Taken from the innards of the sonos package – couldn't get to it, so here we are.
-function parseDIDL(didl) {
+// Taken from the innards of the sonos package – didn't figure out how to get to it, so here we are for now.
+Bellman.parseDIDL(didl) {
   var item;
 
   if ((!didl) || (!didl['DIDL-Lite']) || (!util.isArray(didl['DIDL-Lite'].item)) || (!didl['DIDL-Lite'].item[0])) return {};
@@ -169,3 +182,5 @@ function parseDIDL(didl) {
     albumArtURI : util.isArray(item['upnp:albumArtURI']) ? item['upnp:albumArtURI'][0] : null
   };
 };
+
+module.exports = Bellman;
