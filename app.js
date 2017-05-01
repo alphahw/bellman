1643cf4623536350cdb946c59d37058d29de3b69
Albin Wilén <albin@your-majesty.com>
2017-05-01 18:01:35 -0400

    Config JSON support; event emission on track change; tabs to spaces

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var Bellman = require('./lib/bellman');
+var BellmanInstance = new Bellman(require('./config/config.json'));
 
-Bellman.listen();
+BellmanInstance.listen();
+
+BellmanInstance.on('newTrack', (newTrack) => {
+  console.log('New track! '+JSON.stringify(newTrack));
+});
